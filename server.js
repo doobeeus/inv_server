3d919bddfedebeb8889668dd1430b54310443588
Aaron Ding <doobee@benizzszz>
2023-06-13 15:45:38 -0400

    user models

--- server.js
+++ server.js
@@ -3,16 +3,20 @@ const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const cors = require("cors");
+const userRoute = require("./routes/userRoute");
 
 const app = express();
 
 const PORT = process.env.PORT || 5000;
 
-//middlewares
+// middlewares
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false}));
 app.use(bodyParser.json());
+
+// routes middleware
+app.use("/api/users", userRoute);
 
 // routes
 app.get("/", (req, res) => {
